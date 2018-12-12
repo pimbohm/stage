0272b0f79bec9911057254427526dea33d3d342e
pimbohm <43641554+pimbohm@users.noreply.github.com>
2018-12-12 13:15:18 +0100

    Add files via upload

---
+++ icoon.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Icoon</title>
+</head>
+
+<body>
+    <form method="post" action="upload.php" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <input type="submit" name="submit" value="upload">
+    </form>
+</body>
+
+</html>
